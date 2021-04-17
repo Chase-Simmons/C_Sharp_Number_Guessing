21fc7ee6e57e2f2cc0f09397d905622907974587
Chase Simmons <csim6890@gmail.com>
2021-04-16 22:43:28 -0500

    loop function to check for correct res

--- CsharpIntroApp/Program.cs
+++ CsharpIntroApp/Program.cs
@@ -6,22 +6,50 @@ namespace CsharpIntroApp
     {
         public static void Main(string[] args)
         {
-
+            //DETAILS
             string appName = "Number Guesser";
             string appVersion = "1.0.0";
             string appAuthor = "Chase Simmons";
 
+            //VARS TO BE USED
+            string userName;
+            int userGuess = 0;
+            int correctNumber = 7;
+
+
+            //STARTUP PREP
             Console.ForegroundColor = ConsoleColor.Green;
 
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
 
             Console.ResetColor();
 
-            Console.Write("What is your name? :");
+            //SEEK RES
+            Console.Write("What is your name? : ");
 
-            string userInput = Console.ReadLine();
+            //GRAB RES
+            userName = Console.ReadLine();
 
-            Console.WriteLine("Hello {0}, let's play a game...", userInput);
+            Console.WriteLine("Hello {0}, let's play a game...", userName);
+
+            Console.Write("Guess a number between 1 and 10 : ");
+
+
+           while (userGuess != correctNumber)
+            {
+                string input = Console.ReadLine();
+
+                userGuess = Int32.Parse(input);
+
+                if(userGuess != correctNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Wrong number, please try again!");
+                }
+                    Console.ResetColor();
+
+            }
         }
     }
 }
