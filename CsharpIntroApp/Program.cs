6667d85518a0a031126a91a58cfb82994e4a1b87
Chase Simmons <csim6890@gmail.com>
2021-04-16 23:07:22 -0500

    code cleanup

--- CsharpIntroApp/Program.cs
+++ CsharpIntroApp/Program.cs
@@ -6,23 +6,13 @@ namespace CsharpIntroApp
     {
         public static void Main(string[] args)
         {
-            //DETAILS
-            string appName = "Number Guesser";
-            string appVersion = "1.0.0";
-            string appAuthor = "Chase Simmons";
+
+            GetAppInfo();
 
             //VARS TO BE USED
             string userName;
             int userGuess = 0;
             int correctNumber = new Random().Next(1, 10);
-
-
-            //STARTUP PREP
-            Console.ForegroundColor = ConsoleColor.Green;
-
-            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
-
-            Console.ResetColor();
 
             Console.Write("What is your name? : ");
 
@@ -42,31 +32,19 @@ namespace CsharpIntroApp
 
                     if (!int.TryParse(input, out userGuess))
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
-
-                        Console.WriteLine("Not a number, please try again!");
-
-                        Console.ResetColor();
+                        NotNumber();
                     }
 
                     userGuess = Int32.Parse(input);
 
                     if (userGuess != correctNumber)
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
-
-                        Console.WriteLine("Wrong number, please try again!");
-
-                        Console.ResetColor();
+                        WrongNumber();
                     }
 
                 }
 
-                Console.ForegroundColor = ConsoleColor.Yellow;
-
-                Console.WriteLine("You are CORRECT!!!");
-
-                Console.ResetColor();
+                CorrectNumber();
 
                 Console.Write("Play Again? [Y or N] : ");
 
@@ -79,6 +57,49 @@ namespace CsharpIntroApp
                     return;
                 }
             }
+        }
+
+        static void GetAppInfo()
+        {
+            //DETAILS
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Chase Simmons";
+
+            //STARTUP PREP
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            Console.ResetColor();
+
+        }
+
+        static void CorrectNumber()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("You are CORRECT!!!");
+
+            Console.ResetColor();
+        }
+
+        static void WrongNumber()
+        { 
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Wrong number, please try again!");
+
+            Console.ResetColor();
+        }
+
+        static void NotNumber()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Not a number, please try again!");
+
+            Console.ResetColor();
         }
     }
 }
